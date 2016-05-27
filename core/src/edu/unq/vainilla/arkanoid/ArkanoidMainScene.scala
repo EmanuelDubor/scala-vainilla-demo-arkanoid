3ae5375cbfc5116497546edab88d7454789018b1
EmanuelDubor <emanueldubora@hotmail.com>
2016-05-27 13:24:28 -0300

    Minor updates

--- core/src/edu/unq/vainilla/arkanoid/ArkanoidMainScene.scala
+++ core/src/edu/unq/vainilla/arkanoid/ArkanoidMainScene.scala
@@ -2,7 +2,6 @@ package edu.unq.vainilla.arkanoid
 
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
-import com.badlogic.gdx.math.Vector3
 import edu.unq.vainilla.core.gamescene.SimpleGameScene
 import edu.unq.vainilla.core.input.{SimpleInputHandler, TouchDown}
 
@@ -17,15 +16,15 @@ class ArkanoidMainScene extends SimpleGameScene with SimpleInputHandler {
     logo = new Texture("badlogic.jpg")
   }
 
-  override def render(implicit spriteBatch: SpriteBatch, deltaTime: Float): Unit = {
+  override def render(implicit spriteBatch: SpriteBatch): Unit = {
     super.render
     spriteBatch.draw(logo, imgX, imgY)
   }
 
   override def touchDown: PartialFunction[TouchDown, Unit] = {
-    case TouchDown(x, y, _, _) =>
-      val vector = camera.unproject(new Vector3(x, y, 0))
-      imgX = vector.x
-      imgY = vector.y
+    case TouchDown(screenCords, _, _) =>
+      val cords = screenCords.localize
+      imgX = cords.x
+      imgY = cords.y
   }
 }
